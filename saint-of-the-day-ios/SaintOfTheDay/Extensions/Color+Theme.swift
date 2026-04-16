30f0143b24d6b94f6a2dccf222f861d43c118892
augus <augus@MacBook-Pro-2.local>
2026-04-16 11:45:56 -0400

    Fix asset color collisions and widget bundle metadata

--- saint-of-the-day-ios/SaintOfTheDay/Extensions/Color+Theme.swift
+++ saint-of-the-day-ios/SaintOfTheDay/Extensions/Color+Theme.swift
@@ -1,9 +1,5 @@
 import SwiftUI
 
 extension Color {
-    static let parchment    = Color("ParchmentBeige")
-    static let ancientGold  = Color("AncientGold")
-    static let inkBrown     = Color("InkBrown")
-    static let vellumShadow = Color("VellumShadow")
-    static let frescoRed    = Color("FrescoRed")
+    static let parchment = Color("ParchmentBeige")
 }
